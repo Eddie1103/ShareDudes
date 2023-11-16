eb224b840fe123983f4add9dc7c116f9311f7483
eloth <eliaspascal2503@gmail.com>
2023-11-16 13:30:38 +0100

    Button

--- ShareDudesAdminTool/ShareDudesAdminTool/Login.cs
+++ ShareDudesAdminTool/ShareDudesAdminTool/Login.cs
@@ -6,5 +6,10 @@ namespace ShareDudesAdminTool
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
