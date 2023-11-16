4ceb1b41e0b57d23983a2db802b39f91dbdbd44f
Eddie1103 <Admin@STUDIOEDDIE>
2023-11-16 13:11:33 +0100

    dddd

--- listener.py
+++ listener.py
@@ -1,16 +1,31 @@
+import psycopg2
+from database import Database
 from flask import Flask, request, jsonify
+
+DATABASE = psycopg2._T_conn(Database.connect())
 
 app = Flask(__name__)
 
 @app.route('/request_database', methods=['GET'])
-def extract_text():
-    
-    sqlcommand = request.args.get('methode')
-    
+def request_database():
 
+    methode = request.args.get('methode')
+    columns = request.args.get('parameters')
+    values = request.args.get('values')
 
-    return jsonify({"error": "coming son"}), 403
+    if methode=='get':
+        return jsonify({"result": select()})
+    elif methode =='set':
+        return jsonify({"result": insert(columns, values)})
+    else:
+        return jsonify({"error": "coming soon"}), 403
 
-    
+def select():
+    return None
+
+def insert():
+    return None
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    
