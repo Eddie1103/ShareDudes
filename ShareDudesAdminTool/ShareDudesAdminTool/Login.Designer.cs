8c6e9561f525301691085ba64705bff6057020ed
eloth <eliaspascal2503@gmail.com>
2023-11-16 13:55:40 +0100

    test

--- ShareDudesAdminTool/ShareDudesAdminTool/Login.Designer.cs
+++ ShareDudesAdminTool/ShareDudesAdminTool/Login.Designer.cs
@@ -28,17 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tb_benutzer = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tb_benutzer
             // 
-            this.textBox1.Location = new System.Drawing.Point(248, 205);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(298, 27);
-            this.textBox1.TabIndex = 1;
+            this.tb_benutzer.Location = new System.Drawing.Point(248, 205);
+            this.tb_benutzer.Name = "tb_benutzer";
+            this.tb_benutzer.Size = new System.Drawing.Size(298, 27);
+            this.tb_benutzer.TabIndex = 1;
+            this.tb_benutzer.TextChanged += new System.EventHandler(this.tb_benutzer_TextChanged);
             // 
             // button1
             // 
@@ -46,26 +47,26 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(138, 29);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Login";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox2
+            // tb_password
             // 
-            this.textBox2.Location = new System.Drawing.Point(248, 273);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.PasswordChar = '*';
-            this.textBox2.Size = new System.Drawing.Size(298, 27);
-            this.textBox2.TabIndex = 3;
+            this.tb_password.Location = new System.Drawing.Point(248, 273);
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(298, 27);
+            this.tb_password.TabIndex = 3;
+            this.tb_password.TextChanged += new System.EventHandler(this.tb_password_TextChanged);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.tb_password);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tb_benutzer);
             this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
@@ -74,8 +75,8 @@
         }
 
         #endregion
-        private TextBox textBox1;
+        private TextBox tb_benutzer;
         private Button button1;
-        private TextBox textBox2;
+        private TextBox tb_password;
     }
 }
--- ShareDudesAdminTool/ShareDudesAdminTool/Login.cs
+++ ShareDudesAdminTool/ShareDudesAdminTool/Login.cs
@@ -5,11 +5,20 @@ namespace ShareDudesAdminTool
         public Login()
         {
             InitializeComponent();
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void tb_benutzer_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void tb_password_TextChanged(object sender, EventArgs e)
+        {
+            if(tb_benutzer.Text != "")
+            {
+           
+            }
         }
     }
 }
