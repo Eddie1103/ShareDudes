8aefd5e2943791d0b8971f63673cae59aa7b4236
Eli <eliaspascal2503@gmail.com>
2024-01-16 11:05:30 +0100

    created class reports

---
+++ ShareDudesAdminTool/ShareDudesAdminTool/Reports.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareDudesAdminTool
+{
+    public sealed class Reports
+    {
+        public string _reportText;
+        public string _reportTitle;
+
+        public Reports()
+        {
+            _reportText = string.Empty;
+            _reportTitle = string.Empty;
+        }
+
+        public string ReportText
+        {
+            get { return _reportText; }
+            set { _reportText = value; }
+        }
+
+        public string ReportTitle
+        {
+            get { return _reportTitle; }
+            set { _reportTitle = value; }
+        }
+    }
+}
