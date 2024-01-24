3036db4a4ec8b3f18d5ac236f80f3eacb0fe57b2
BuildTools <unconfigured@null.spigotmc.org>
2024-01-24 16:04:30 +0100

    added api connection inserate

--- dynamicscript.js
+++ dynamicscript.js
@@ -1,37 +1,20 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var jsonData = [
-        {
-            title: "Habe Rouladen übrig! ",
-            text: "Habe eine größere Portion gekocht aber der Hunger war größer als der Magen, würde so 3 Stück abgegeben!",
-            pic: "https://bbq-piraten.de/community/data/attachments/351/351068-07b61497a93c18894bdb5a9aa675b686.jpg"
+    $.ajax({
+        url: 'http://194.62.1.218:5000/getinserate',
+        method: 'GET',
+        dataType: 'json',
+        success: function(response) {
+            HandleResponse(response);
         },
-        {
-            title: "Waschmaschine zu verleihen",
-            text: "Hallo, ich würde gerne meine Waschmaschine zur Verfügung stellen während ich im Urlaub bin, einfach anfragen!",
-            pic: "https://img.kleinanzeigen.de/api/v1/prod-ads/images/e2/e27767ff-fadb-4430-9d68-c9beec8f9ecf?rule=$_59.JPG"
-        },
-        {
-            title: "Gartenbeet frei!",
-            text: "Hallihallo, habe in meinem Garten noch ein Beet frei, zuletzt waren dort Tomaten gepflanzt aber dieses Jahr habe ich keine Lust. Wenn jemand an diesem Platz anpflanzen möchte einfach melden!",
-            pic: "https://upload.wikimedia.org/wikipedia/commons/a/ab/Schrebergarten_1.jpg"
-        },
-        {
-            title: "Linus brauch einen Aufpasser",
-            text: "Hey Community, mein Hund Linus ist eine Woche auf sich allein gestellt da ich im Urlaub bin, bevor ich Angehörige nerve, frage ich ob sich hier vielleicht jemand findet! Mehr Infos nach Anfrage.",
-            pic: "https://www.zooroyal.de/magazin/wp-content/uploads/2017/08/hund-in-der-spaziergang-760x570.jpg"
-        },
-        {
-            title: "Verschenke rostige Mühle",
-            text: "Kein Bock mehr auf das Fahrrad in meinem Keller, steht nur rum und nimmt Platz weg. Wer es gerne haben möchte meldet sich.",
-            pic: "https://cdn.germanscooterforum.de/monthly_04_2008/post-5890-1207589007_thumb.jpg"
-        },
-        {
-            title: "Alte Musikanlage kaputt",
-            text: "Meine alte Stereoanlage macht Probleme, kann jemand mit Kentnissen mir helfen? Entlohnung folgt Selbstverständlich.",
-            pic: "https://light-microscope.net/wp-content/uploads/2019/05/Steroanlage.jpg"
+        error: function(error) {
+          alert(error);
         }
-    ];
+      });
+});
+
+function HandleResponse(data){
+    var jsonData = data;
 
     var cardContainer = document.getElementById('cardContainer');
     cardContainer.innerHTML = '';
@@ -56,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
     function createCard(data) {
         var card = document.createElement('div');
         card.className = 'card';
+        card.id = data.id;
 
         var cardBody = document.createElement('div');
         cardBody.className = 'card-body d-flex flex-column';
@@ -69,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function () {
         cardText.textContent = data.text;
 
         var cardPic = document.createElement('img');
-        cardPic.src = data.pic;
+        cardPic.src = data.image_base64;
         cardPic.classname = 'card-img-top';
 
         var cardButton = document.createElement('a');
@@ -96,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
         col.appendChild(card);
         return col;
     }
-});
+}
 
 function loadInseratData(inseratTitle, inseratText, inseratPic) {
     var title = $("#modalTitle");
